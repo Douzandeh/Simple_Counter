093de000709afdc85e0389b61a74896cc7a71b24
Hossein Douzandeh <douzandeh.k@gmail.com>
2025-11-04 13:21:03 +0330

    Counter component structure

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -5,15 +5,27 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="container">
       <h1>Counter</h1>
-      <div>
-        <span></span>
+      <div className="display">
+        <span>{count}</span>
       </div>
-      <div>
-        <button>+</button>
-        <button>-</button>
-        <button>Reset</button>
+      <div className="button-group">
+        <button
+          className="button increment"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+        <button
+          className="button decrement"
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
+        <button className="button reset" onClick={() => setCount(0)}>
+          Reset
+        </button>
       </div>
     </div>
   );
