fd9c797d6dc911500c1028bd29a0a0aa7ad05511
Hossein Douzandeh <douzandeh.k@gmail.com>
2025-11-04 13:09:43 +0330

    The basic structure of the Counter components and App

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,8 +1,22 @@
-import React from "react";
-import ".Counter.css";
+import React, { useState } from "react";
+import "./Counter.css";
 
 function Counter() {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <div>
+        <span></span>
+      </div>
+      <div>
+        <button>+</button>
+        <button>-</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
 }
 
 export default Counter;
